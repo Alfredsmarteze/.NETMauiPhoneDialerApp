8df96a9e2f3fb6c2f080ad2c5c760ad6b27ab92d
alfredsmarteze <alfredsmarteze@outlook.com>
2022-08-20 15:55:40 +0100

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	string translatedNumber;
+
+	private void OnTranslate(object sender, EventArgs args)
+	{ 
+		string enterNUmber=PhoneNumberText.Text;
+
+		translatedNumber = MauiApp1.PhoneNumberDialer.ToNumber(enterNUmber);
+
+		if (!string.IsNullOrEmpty(translatedNumber))
+		{
+			CallButton.IsEnabled = true;
+			CallButton.Text = $"Call {translatedNumber} ";
+		}
+		else
+		{
+			CallButton.IsEnabled=false;
+			CallButton.Text = "Call";
+		}
+	}
+
+	async void OnCall(object sender, EventArgs args)
+	{
+		if (await this.DisplayAlert("Dial a number", "Would you like to call" + translatedNumber + "?","Yes", "No" ))
+		{
+			try
+			{
+				if (PhoneDialer.Default.IsSupported)
+				{
+					PhoneDialer.Default.Open(translatedNumber);
+				}
+			}
+			catch (ArgumentNullException)
+			{
+				await DisplayAlert("Unable to dial", "Phone number was not valid", "Ok");
+			}
+			catch(Exception) { await DisplayAlert("Unable to dial", "Phone dialing failed", "Ok"); }
+		}
+	}
+
+	//private void OnCounterClicked(object sender, EventArgs e)
+	//{
+	//	count++;
+
+	//	if (count == 1)
+	//		CounterBtn.Text = $"Clicked {count} time";
+	//	else
+	//		CounterBtn.Text = $"Clicked {count} times";
+
+	//	SemanticScreenReader.Announce(CounterBtn.Text);
+	//}
+}
+
---
+++ MauiApp1/PhoneNumberDialer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public static class PhoneNumberDialer
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return raw;
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var item in raw)
+            {
+                if ("-0123456789".Contains(item))
+                    newNumber.Append(item);
+                else
+                {
+                    var result = TranslateToNumber(item);
+                    if (result != null)
+                        newNumber.Append(result);
+                    else return null;
+                }
+            }
+             return newNumber.ToString();
+        }
+        static readonly string[] digits = { "ABC","DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                
+                    return 2+i;
+            }
+            return null;
+        }
+    }
+    
+}
